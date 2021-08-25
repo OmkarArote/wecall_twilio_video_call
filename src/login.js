77e8fbbece9f3d0a2330f3e94b1395d714178b88
Omkar <omkararote0@gmail.com>
2021-08-25 16:44:07 +0530

    Added Login Regx Validation

--- src/login.js
+++ src/login.js
@@ -5,53 +5,93 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
 import { AuthContext } from './context';
 import * as Animatable from 'react-native-animatable';
+import Toast from 'react-native-simple-toast';
 
 export default function Login({navigation}) {
-  const [data, setData] =  React.useState({secureTextEntry: true});
+  const [data, setData] =  React.useState({
+    check_textInputChange: false,
+    secureTextEntry: true,
+    isValidUser: true,
+    isValidPassword: true,
+  });
+  const [useremail, setEmail] = useState('');
+  const [userpass, setPass] = useState('');
   const updateSecureTextEntry = () => {
     setData({
         secureTextEntry: !data.secureTextEntry
     });
   }
-  const [useremail, setEmail] = useState('');
-  const [userpass, setPass] = useState('');
-  const { signIn } = React.useContext(AuthContext);
-    const login = (useremail, userpass) =>{
-      var URL_LOGIN = 'https://wecall-api-infocicion.herokuapp.com/user';
-      fetch(URL_LOGIN, {
-        method: 'POST',
-        headers:{
-          'Content-Type': 'application/json',
-          'Authorization': 'Basic '+btoa('admin:admin')
-        },
-        body: JSON.stringify({
-          user_email_address: useremail,
-          user_password: userpass
-        })      
-      })
-      .then(
-        function(response){
-          if (response.status == 200) {
-            console.log('Login Succesful')
-            signIn()
-          }
-          else{
-            console.log('Login Unsuccesful')
-          }
-        }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error', err);
+  const handleValidUser = (val) => {
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    let validemailformat= reg.test(val)
+    if(validemailformat){
+      setData({
+        ...data,
+        isValidUser: true
+      });
+    } else {
+      console.log("Warning: Email Format Is Incorrect")
+      setData({
+        ...data,
+        isValidUser: false
       });
     }
-    return (
-    <View style={styles.mainContainer}>
-      <Text style={{color:'#d40000',fontWeight:'bold',fontSize:27,paddingTop:30}}>SIGN IN</Text>
-      <View style={styles.container}>
-        <Animatable.Image style={{height:250,width:250}} animation="bounceIn"
-            source={require('../assets/logo.png')}
-        />
-        <View style={styles.contentWrapper}>
+  }
+  const handleValidPass = (val) => {
+    if(val.length >= 8){
+      setData({
+        ...data,
+        isValidPassword: true
+      });
+    } else {
+      console.log("Warning: Password is too short")
+      setData({
+        ...data,
+        isValidPassword: false
+      });
+    }
+  }
+  const { signIn } = React.useContext(AuthContext);
+  const login = (useremail, userpass) =>{
+    console.log("Final Entered Email: "+useremail)
+    console.log("Final Entered Password: "+userpass)
+    var URL_LOGIN = 'https://wecall-api-infocicion.herokuapp.com/user';
+    fetch(URL_LOGIN, {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic '+btoa('admin:admin')
+      },
+      body: JSON.stringify({
+        user_email_address: useremail,
+        user_password: userpass
+      })      
+    })
+    .then(
+      function(response){
+        if (response.status == 200) {
+          Toast.show('Login Successful', Toast.LONG);
+          console.log('Login Successful')
+          signIn()
+        }
+        else{
+          Toast.show('User Email or Password is Incorrect', Toast.LONG);
+          console.log('Login Unsuccessful')
+        }
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error', err);
+    });
+  }
+  return (
+  <View style={styles.mainContainer}>
+    <Text style={{color:'#d40000',fontWeight:'bold',fontSize:27,paddingTop:30}}>SIGN IN</Text>
+    <View style={styles.container}>
+      <Animatable.Image style={{height:250,width:250}} animation="bounceIn"
+          source={require('../assets/logo.png')}
+      />
+      <View style={styles.contentWrapper}>
         <Animatable.View  animation="fadeInUpBig" style={styles.action} >
             <Icon 
                 name="envelope-o"
@@ -67,10 +107,16 @@ export default function Login({navigation}) {
                 }]}
                 autoCapitalize="none"
                 onChangeText = {useremail => setEmail(useremail)}
+                onEndEditing ={(p)=>handleValidUser(p.nativeEvent.text)}
             />
         </Animatable.View>
+        {data.isValidUser ? null :
+        <Animatable.View animation="fadeInLeft" duration={500}>
+        <Text style={[styles.errorMsg]}>Please check the email format</Text>
+        </Animatable.View>
+        }
         <Animatable.View style={[styles.action,{
-                marginTop: 27}]} animation="fadeInUpBig">
+                marginTop: 13}]} animation="fadeInUpBig">
             <Icon 
                 name="key"
                 color="#666666"
@@ -85,7 +131,8 @@ export default function Login({navigation}) {
                 }]}
                 autoCapitalize="none"
                 onChangeText = {userpass => setPass(userpass)}
-                secureTextEntry={data.secureTextEntry ? true : false}             
+                onEndEditing ={(e)=>handleValidPass(e.nativeEvent.text)}
+                secureTextEntry={data.secureTextEntry ? true : false}
             />
             <TouchableOpacity onPress={updateSecureTextEntry}>
                     {data.secureTextEntry ? 
@@ -105,36 +152,40 @@ export default function Login({navigation}) {
                     }
             </TouchableOpacity>
         </Animatable.View>
-        </View>
-        <Animatable.View animation="fadeInUpBig">
-          <TouchableOpacity style={styles.button} onPress={() => login(useremail, userpass)}>
-            <Text style={styles.buttonText}>LOG IN</Text>
-          </TouchableOpacity>
+        {data.isValidPassword ? null :
+        <Animatable.View animation="fadeInLeft" duration={500}>
+        <Text style={[styles.errorMsg]}>Please length must be greater the 8</Text>
         </Animatable.View>
-        <Animatable.View animation="fadeInUpBig" style={styles.hyperLinkText}>
-          <TouchableOpacity >
-              <Text style={styles.hyperLink} onPress={() => navigation.navigate('Forgot_Password')}>Forgot Password?</Text>
-          </TouchableOpacity>
-          <View style={{flexDirection:'row'}}>
-            <Text>Don't have an account?</Text>
-            <TouchableOpacity  onPress={() => navigation.navigate('Register')}>
-              <Text style={styles.hyperLink}> Sign up</Text>
-            </TouchableOpacity>
-          </View>
-        </Animatable.View>
-        <Animatable.View animation="fadeInUpBig" style={styles.infocisionLogoContainer}>
-            <Image
-                style={styles.infocisionLogo}
-                source={require('../assets/Infocision-Logo.png')}
-            />
-            <Text>www.infocision.in</Text>
-        </Animatable.View>
+        }
       </View>
-      <StatusBar style="auto" />
+      <Animatable.View animation="fadeInUpBig">
+        <TouchableOpacity style={styles.button} onPress={() => login(useremail, userpass)}>
+          <Text style={styles.buttonText}>LOG IN</Text>
+        </TouchableOpacity>
+      </Animatable.View>
+      <Animatable.View animation="fadeInUpBig" style={styles.hyperLinkText}>
+        <TouchableOpacity >
+            <Text style={styles.hyperLink} onPress={() => navigation.navigate('Forgot_Password')}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <View style={{flexDirection:'row'}}>
+          <Text>Don't have an account?</Text>
+          <TouchableOpacity  onPress={() => navigation.navigate('Register')}>
+            <Text style={styles.hyperLink}> Sign up</Text>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
+      <Animatable.View animation="fadeInUpBig" style={styles.infocisionLogoContainer}>
+          <Image
+              style={styles.infocisionLogo}
+              source={require('../assets/Infocision-Logo.png')}
+          />
+          <Text>www.infocision.in</Text>
+      </Animatable.View>
     </View>
+    <StatusBar style="auto" />
+  </View>
   );
 }
-
 const styles = StyleSheet.create({
   mainContainer:{
     flexGrow:1,  
@@ -150,7 +201,6 @@ const styles = StyleSheet.create({
   inputWrap:{
     flexDirection:'row',
   },
- 
   ImageStyle: {
       padding: 10,
       margin: 5,
@@ -172,7 +222,6 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.29,
     shadowRadius: 4.65,
-
     elevation: 7,
   },
   buttonText: {
@@ -189,6 +238,11 @@ const styles = StyleSheet.create({
   hyperLink:{
       color: '#a40b0e',
       fontSize:14,
+  },
+  errorMsg:{
+    color: '#ff0000',
+    fontWeight: '600',
+    marginTop: 2
   },
   infocisionLogoContainer:{
     marginTop:60,
